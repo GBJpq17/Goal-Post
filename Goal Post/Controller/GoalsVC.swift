6b2eff97066bf8c5d36d6125a4f87d7721db41a7
Ryan Loera <rloera@csumb.edu>
2017-09-03 12:29:46 -0400

    updated GoalsVC interface to default view

---
+++ Goal Post/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  GoalsVC.swift
+//  Goal Post
+//
+//  Created by RyLo on 9/3/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func goalButtonWasPressed(_ sender: Any) {
+    }
+    
+}
+
