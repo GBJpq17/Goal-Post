73a447964e75fd5dbdd0fe60fd6861808536a914
Ryan Loera <rloera@csumb.edu>
2017-09-04 11:41:43 -0400

    creted UIViewControllerExt to make the ViewController move like a NavigationController

--- Goal Post/Controller/CreateGoalVC.swift
+++ Goal Post/Controller/CreateGoalVC.swift
@@ -30,7 +30,7 @@ class CreateGoalVC: UIViewController {
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+        dismissDetail()
     }
     
     
