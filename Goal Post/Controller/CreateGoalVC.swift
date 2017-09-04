e3ccdf6389442554e036dcd621e7ce2c42f5a1a9
Ryan Loera <rloera@csumb.edu>
2017-09-04 11:30:01 -0400

    added CreateGoalsVC to display set up for goals

---
+++ Goal Post/Controller/CreateGoalVC.swift
@@ -0,0 +1,38 @@
+//
+//  CreateGoalVC.swift
+//  Goal Post
+//
+//  Created by RyLo on 9/4/17.
+//  Copyright © 2017 GBJpq. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermButton: UIButton!
+    @IBOutlet weak var longTermButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    @IBAction func nextButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func shortTermButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermButtonPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backButtonPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    
+}
