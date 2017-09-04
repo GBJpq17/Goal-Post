7b15f14e0c8e4e6967cae231617c5769a476b9ac
Ryan Loera <rloera@csumb.edu>
2017-09-04 12:31:54 -0400

    created extension for UIView to move the NEXT button up when user starts typing goal, created extension for UIButton to change color of short term and long term buttons when pressed

--- Goal Post/Controller/CreateGoalVC.swift
+++ Goal Post/Controller/CreateGoalVC.swift
@@ -14,19 +14,29 @@ class CreateGoalVC: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    var goalType: goalType = .shortTerm
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func nextButtonPressed(_ sender: Any) {
     }
     
     @IBAction func shortTermButtonPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func longTermButtonPressed(_ sender: Any) {
+        goalType = .longTerm
+        shortTermButton.setDeselectedColor()
+        longTermButton.setSelectedColor()
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
